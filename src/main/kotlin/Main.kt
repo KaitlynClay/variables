1fdebc66c7e4ce953d4d6ef5eab00182f9142bc2
Kaitlyn Clay <kclay@students.mchenry.edu>
2023-08-22 09:58:15 -0500

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+import java.security.KeyStore.TrustedCertificateEntry
+
+fun main(args: Array<String>) {
+    // Numbers
+    var Int1: Int = 13
+    var Long1 = 75L
+    var Float1: Float = 3.14F
+    var Double1: Double = 105.69873
+
+    var Int2 = 58
+
+    //Booleans
+    var Boolean1: Boolean = true
+    Boolean1 = false
+
+    //Strings
+    var String1 = "Misc sentence string!"
+    var String2 = "Secondary misc sentence string!! \n"
+    var multiLines1 = """
+        Random thing 1
+        Random thing 2
+        Random thing 3
+    """
+
+    val account = 560
+    val acBalance = "Your balance is $account"
+
+}
