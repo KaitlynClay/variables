8aea8f3e16091b44992c9aa4f4b4336550d67145
Kaitlyn Clay <kclay@students.mchenry.edu>
2023-08-22 10:58:22 -0500

    Original

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,12 +6,25 @@ fun main(args: Array<String>) {
     var Long1 = 75L
     var Float1: Float = 3.14F
     var Double1: Double = 105.69873
+    var Short1: Short = 7
+    var Byte1: Byte = 2
 
-    var Int2 = 58
+
+    println("An Int Value is " + Int1);
+    println("A Double  Value is " + Double1);
+    println("A Float Value is " + Float1);
+    println("A Long Value is ${Long1}");
+    println("A Short Value is ${Short1}");
+    println("A Byte Value is ${Byte1}");
+
+    //Char
+    val Char1: Char = 'J'
+    println("A Char value is ${Char1}")
 
     //Booleans
     var Boolean1: Boolean = true
     Boolean1 = false
+    println("A Boolean value is ${Boolean1}")
 
     //Strings
     var String1 = "Misc sentence string!"
@@ -21,8 +34,11 @@ fun main(args: Array<String>) {
         Random thing 2
         Random thing 3
     """
+    println(String2)
+    println("Here is an example of a multiline string ${multiLines1}")
 
     val account = 560
-    val acBalance = "Your balance is $account"
+    val acBalance = "Your balance is $account dollars"
+    println(acBalance)
 
 }
